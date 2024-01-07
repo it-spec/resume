894b24786dc76a615d6c7a59ca0e287e79b70b39
Alexey A. SHaruk <138595197+it-spec@users.noreply.github.com>
2024-01-07 18:11:33 +0600

    4.0

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,10 @@ var sidemenu = document.getElementById("sidemenu");
 
 function openmenu(){
     sidemenu.style.right = "0";
+    sidemenu.style.boxShadow = "-0.01em 0em 5em black";
 }
 
 function closemenu(){
     sidemenu.style.right = "-50%";
+    sidemenu.style.boxShadow = "none";
 }
