3a4af20baab65ca495e4b4d9aeddaee0bb87585e
Alexey A. SHaruk <138595197+it-spec@users.noreply.github.com>
2024-03-30 12:45:48 +0500

    Add pb

--- js/menu.js
+++ js/menu.js
@@ -9,3 +9,18 @@ function closemenu(){
     sidemenu.style.right = "-50%";
     sidemenu.style.boxShadow = "none";
 }
+
+function progressBar() {
+    // Узнаем на сколько страница прокручена
+    let scroll = document.body.scrollTop || document.documentElement.scrollTop;
+    // Узнаем высоту всей страницы
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    // Получаем в % на сколько прокручена страница
+    let scrolled = scroll / height * 100;
+
+    // Подставляем % прокрутки в ширину нашей линии
+    document.getElementById('progressBar').style.width = scrolled + '%';
+}
+
+// Запускаем функцию, когда пользователя скролит
+window.addEventListener('scroll', progressBar);
